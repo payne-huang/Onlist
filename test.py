7cf8c3796c3b8e8325c848ad504c65c9f9b7388e
HuangSheng <sheng.huang@56qq.com>
2019-04-03 17:53:38 +0800

    处理异常后重新请求数据

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import os
 
-s = "dfd 333[ffff()].dddd.mkv"
-print os.path.splitext(s)[1]
+from operator import itemgetter, attrgetter
+def cmpx(x, y):
+
+    return x[0]-y[0]
+s = [{'a':u'2019-01-31T02:53:32Z'},{'a':u'2019-01-30T02:53:32Z'}]
+print sorted(s,  key=lambda x:x["a"], reverse=True)
+
